875ec8bf39a6b6cdfd5e7a1d71d0b5e3be8b7184
Anthony Corletti <anthcor@gmail.com>
2022-12-23 17:33:30 -0500

    wip

--- 2022/day18/src/main.rs
+++ 2022/day18/src/main.rs
@@ -1,3 +1,121 @@
 fn main() {
-    println!("Hello, world!");
+    let input_str = include_str!("../input.txt");
+    let content = Input::from_string(input_str);
+    println!("Part 1: {}", part1(&content));
+    println!("Part 2: {}", part2(&content));
+}
+
+fn part1(content: &Input) -> usize {
+    let mut exposed = 0;
+
+    for cube in &content.cubes {
+        let neighbors = cube.get_neighbors();
+
+        exposed += 6 - content
+            .cubes
+            .iter()
+            .filter(|c| *c != cube)
+            .filter(|c| neighbors.contains(c))
+            .count();
+    }
+
+    exposed
+}
+
+fn part2(content: &Input) -> usize {
+    let cubes: Vec<Cube> = content
+        .cubes
+        .iter()
+        .map(|c| Cube::new(c.x + 1, c.y + 1, c.z + 1))
+        .collect();
+
+    let x = content.cubes.iter().map(|cube| cube.x).max().unwrap() + 2;
+    let y = content.cubes.iter().map(|cube| cube.y).max().unwrap() + 2;
+    let z = content.cubes.iter().map(|cube| cube.z).max().unwrap() + 2;
+
+    count_faces(&cubes, x, y, z)
+}
+
+fn count_faces(cubes: &Vec<Cube>, x: i32, y: i32, z: i32) -> usize {
+    let mut queue: Vec<Cube> = vec![Cube::new(0, 0, 0)];
+    let mut visited: Vec<Cube> = vec![];
+    let mut faces = 0;
+
+    while let Some(cursor) = queue.pop() {
+        // get the cells around the cursor
+        let around = cursor
+            .get_neighbors()
+            .into_iter()
+            .filter(|cube| {
+                cube.x >= 0
+                    && cube.y >= 0
+                    && cube.z >= 0
+                    && cube.x <= x
+                    && cube.y <= y
+                    && cube.z <= z
+            })
+            .collect::<Vec<Cube>>();
+
+        // count how many neighbours are actually cubes, these are faces we can count
+        faces += around.iter().filter(|c| cubes.contains(c)).count();
+
+        // queue up unvisited neighbours
+        let mut next = around
+            .into_iter()
+            .filter(|c| !cubes.contains(c))
+            .filter(|c| !visited.contains(c))
+            .filter(|c| !queue.contains(c)) // don't forget the queue or we'll
+            .collect(); // visit locations more than once
+
+        queue.append(&mut next);
+        visited.push(cursor);
+    }
+
+    faces
+}
+
+struct Input {
+    cubes: Vec<Cube>,
+}
+
+impl Input {
+    fn from_string(s: &str) -> Self {
+        Input {
+            cubes: s.lines().map(Cube::from_string).collect(),
+        }
+    }
+}
+
+#[derive(PartialEq)]
+struct Cube {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Cube {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        Cube { x, y, z }
+    }
+
+    fn from_string(s: &str) -> Self {
+        let mut line = s.split(',').into_iter();
+
+        Cube::new(
+            line.next().unwrap().parse().unwrap(),
+            line.next().unwrap().parse().unwrap(),
+            line.next().unwrap().parse().unwrap(),
+        )
+    }
+
+    fn get_neighbors(&self) -> Vec<Cube> {
+        vec![
+            Cube::new(self.x + 1, self.y, self.z),
+            Cube::new(self.x - 1, self.y, self.z),
+            Cube::new(self.x, self.y + 1, self.z),
+            Cube::new(self.x, self.y - 1, self.z),
+            Cube::new(self.x, self.y, self.z + 1),
+            Cube::new(self.x, self.y, self.z - 1),
+        ]
+    }
 }
--- 2022/day19/src/main.rs
+++ 2022/day19/src/main.rs
@@ -1,3 +1,297 @@
 fn main() {
-    println!("Hello, world!");
+    let input = Input::from("./input.txt");
+    // println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+fn part2(input: &Input) -> u32 {
+    let foreman = Foreman::new();
+
+    let mut geode_counts = vec![];
+
+    for blueprint in input.blueprints.iter().take(3) {
+        let ore_first = build(Factory::new(), blueprint, &Robot::Ore, &foreman, 32);
+        let clay_first = build(Factory::new(), blueprint, &Robot::Clay, &foreman, 32);
+
+        let geodes = ore_first.max(clay_first);
+        println!("Blueprint {} creates max {} geodes", blueprint.id, geodes);
+
+        geode_counts.push(geodes);
+    }
+
+    geode_counts.into_iter().product()
+}
+
+fn part1(input: &Input) -> u32 {
+    let foreman = Foreman::new();
+
+    let mut quality_levels = vec![];
+
+    for blueprint in &input.blueprints {
+        let ore_first = build(Factory::new(), blueprint, &Robot::Ore, &foreman, 24);
+        let clay_first = build(Factory::new(), blueprint, &Robot::Clay, &foreman, 24);
+
+        let geodes = ore_first.max(clay_first);
+        println!("Blueprint {} creates max {} geodes", blueprint.id, geodes);
+
+        let quality = blueprint.id * geodes;
+        quality_levels.push(quality);
+    }
+
+    quality_levels.into_iter().sum()
+}
+
+fn build(
+    mut factory: Factory,
+    blueprint: &Blueprint,
+    robot: &Robot,
+    foreman: &Foreman,
+    minutes: u32,
+) -> u32 {
+    // how long it will take to wait for all ingredients to be ready
+    let wait_time = factory.measure_wait_for(robot, blueprint);
+
+    // if we don't have time to wait for the resources for this robot, then tick
+    // the remaining minutes and return the final number of geodes produced
+    if wait_time >= minutes {
+        factory.accrue_gains_for(minutes);
+        return factory.geodes;
+    }
+
+    factory.accrue_gains_for(wait_time);
+
+    // pay for our new robot
+    match robot {
+        Robot::Ore => {
+            factory.ore -= blueprint.ore_robot_ore;
+        }
+        Robot::Clay => {
+            factory.ore -= blueprint.clay_robot_ore;
+        }
+        Robot::Obsidian => {
+            factory.ore -= blueprint.obsidian_robot_ore;
+            factory.clay -= blueprint.obsidian_robot_clay;
+        }
+        Robot::Geode => {
+            factory.ore -= blueprint.geode_robot_ore;
+            factory.obsidian -= blueprint.geode_robot_obsidian;
+        }
+    }
+
+    factory.accrue_gains_for(1);
+
+    match robot {
+        Robot::Ore => {
+            factory.ore_robots += 1;
+        }
+        Robot::Clay => {
+            factory.clay_robots += 1;
+        }
+        Robot::Obsidian => {
+            factory.obsidian_robots += 1;
+        }
+        Robot::Geode => {
+            factory.geode_robots += 1;
+        }
+    }
+
+    if minutes - wait_time - 1 < 1 {
+        return factory.geodes;
+    }
+
+    let mut geode_counts = vec![];
+
+    // we need a robot to build next, try all of them and return the best
+    for robot in foreman.robots_plannable_at(&factory) {
+        let geodes = build(
+            factory.clone(), // this clone is probably our time thief
+            blueprint,
+            robot,
+            foreman,
+            minutes - wait_time - 1,
+        );
+
+        geode_counts.push(geodes);
+    }
+
+    return geode_counts.into_iter().max().unwrap_or(0);
+}
+
+struct Input {
+    blueprints: Vec<Blueprint>,
+}
+
+impl Input {
+    fn from(path: &str) -> Self {
+        let content = std::fs::read_to_string(path).expect("Couldn't read input");
+        Input::from_string(content.trim())
+    }
+
+    fn from_string(s: &str) -> Self {
+        Input {
+            blueprints: s.lines().map(Blueprint::from_string).collect(),
+        }
+    }
+}
+
+struct Blueprint {
+    id: u32,
+    ore_robot_ore: u32,
+    clay_robot_ore: u32,
+    obsidian_robot_ore: u32,
+    obsidian_robot_clay: u32,
+    geode_robot_ore: u32,
+    geode_robot_obsidian: u32,
+}
+
+impl Blueprint {
+    fn from_string(s: &str) -> Self {
+        let re = regex::Regex::new(r"Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.").unwrap();
+        let captures = re.captures(s).unwrap();
+
+        Blueprint {
+            id: captures[1].parse().unwrap(),
+            ore_robot_ore: captures[2].parse().unwrap(),
+            clay_robot_ore: captures[3].parse().unwrap(),
+            obsidian_robot_ore: captures[4].parse().unwrap(),
+            obsidian_robot_clay: captures[5].parse().unwrap(),
+            geode_robot_ore: captures[6].parse().unwrap(),
+            geode_robot_obsidian: captures[7].parse().unwrap(),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Factory {
+    // quantities of each robot
+    ore_robots: u32,
+    clay_robots: u32,
+    obsidian_robots: u32,
+    geode_robots: u32,
+
+    // quantities of rocks on hand
+    ore: u32,
+    clay: u32,
+    obsidian: u32,
+
+    // geodes collected
+    geodes: u32,
+}
+
+impl Factory {
+    fn new() -> Self {
+        Factory {
+            ore_robots: 1,
+            clay_robots: 0,
+            obsidian_robots: 0,
+            geode_robots: 0,
+            ore: 0,
+            clay: 0,
+            obsidian: 0,
+            geodes: 0,
+        }
+    }
+
+    fn measure_wait_for(&self, robot: &Robot, blueprint: &Blueprint) -> u32 {
+        use num_integer::div_ceil;
+
+        match robot {
+            Robot::Ore => {
+                if self.ore >= blueprint.ore_robot_ore {
+                    return 0;
+                }
+            }
+            Robot::Clay => {
+                if self.ore >= blueprint.clay_robot_ore {
+                    return 0;
+                }
+            }
+            Robot::Obsidian => {
+                if self.ore >= blueprint.obsidian_robot_ore
+                    && self.clay >= blueprint.obsidian_robot_clay
+                {
+                    return 0;
+                }
+            }
+            Robot::Geode => {
+                if self.ore >= blueprint.geode_robot_ore
+                    && self.obsidian >= blueprint.geode_robot_obsidian
+                {
+                    return 0;
+                }
+            }
+        }
+
+        match robot {
+            Robot::Ore => div_ceil(blueprint.ore_robot_ore - self.ore, self.ore_robots),
+            Robot::Clay => div_ceil(blueprint.clay_robot_ore - self.ore, self.ore_robots),
+            Robot::Obsidian => {
+                let ore = if self.ore > blueprint.obsidian_robot_ore {
+                    0
+                } else {
+                    blueprint.obsidian_robot_ore - self.ore
+                };
+                let clay = if self.clay > blueprint.obsidian_robot_clay {
+                    0
+                } else {
+                    blueprint.obsidian_robot_clay - self.clay
+                };
+
+                div_ceil(ore, self.ore_robots).max(div_ceil(clay, self.clay_robots))
+            }
+            Robot::Geode => {
+                let ore = if self.ore > blueprint.geode_robot_ore {
+                    0
+                } else {
+                    blueprint.geode_robot_ore - self.ore
+                };
+                let obs = if self.obsidian > blueprint.geode_robot_obsidian {
+                    0
+                } else {
+                    blueprint.geode_robot_obsidian - self.obsidian
+                };
+
+                div_ceil(ore, self.ore_robots).max(div_ceil(obs, self.obsidian_robots))
+            }
+        }
+    }
+
+    fn accrue_gains_for(&mut self, minutes: u32) {
+        self.ore += minutes * self.ore_robots;
+        self.clay += minutes * self.clay_robots;
+        self.obsidian += minutes * self.obsidian_robots;
+        self.geodes += minutes * self.geode_robots;
+    }
+}
+
+struct Foreman {
+    robots: Vec<Robot>,
+}
+
+impl Foreman {
+    fn new() -> Self {
+        Foreman {
+            robots: vec![Robot::Ore, Robot::Clay, Robot::Obsidian, Robot::Geode],
+        }
+    }
+
+    // return a slice of robots that can be built given the robots already on hand
+    fn robots_plannable_at(&self, factory: &Factory) -> &[Robot] {
+        if factory.obsidian_robots > 0 {
+            return &self.robots[..4];
+        }
+        if factory.clay_robots > 0 {
+            return &self.robots[..3];
+        }
+
+        // we can always build Ore and Clay robots
+        return &self.robots[..2];
+    }
+}
+
+enum Robot {
+    Ore,
+    Clay,
+    Obsidian,
+    Geode,
 }
